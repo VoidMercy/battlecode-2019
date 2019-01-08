966967f821d761ece4b0ba043b1f919a9dc5364a
Alex <lin969@purdue.edu>
2019-01-07 22:20:09 -0500

    bigly code

--- bot/robot.js
+++ bot/robot.js
@@ -10,6 +10,7 @@ var pilgrimcount = 0;
 var karblocation = null;
 var builtchurch = false;
 var churchloc = null;
+var castleloc = null;
 
 class MyRobot extends BCAbstractRobot {
 
@@ -21,7 +22,32 @@ class MyRobot extends BCAbstractRobot {
         return this.fuel >= SPECS.UNITS[unit].CONSTRUCTION_FUEL && this.fuel >= SPECS.UNITS[unit].CONSTRUCTION_KARBONITE
     }
 
+    hash(x, y) {
+        return x * 99999 + y;
+    }
+
+    adjacent(loc1, loc2) {
+        if (Math.abs(loc1[0] - loc2[0]) + Math.abs(loc1[1] - loc2[1]) > 2) {
+            return false;
+        }
+        return true;
+    }
+
+    moveto(dest) {
+        var dirvec = [dest[0] - this.me.x, dest[1] - this.me.y];
+        var mag = Math.sqrt(dirvec[0] * dirvec[0] + dirvec[1] * dirvec[1]);
+        dirvec[0] = Math.trunc(dirvec[0] / mag * Math.sqrt(SPECS.UNITS[this.me.unit].SPEED));
+        dirvec[1] = Math.trunc(dirvec[1] / mag * Math.sqrt(SPECS.UNITS[this.me.unit].SPEED));
+        this.log("Fucking move");
+        this.log(this.me.x);
+        this.log(this.me.y);
+        this.log(dest);
+
+        return this.move(dirvec[0], dirvec[1]);
+    }
+
     turn() {
+
 
         if (this.me.unit == SPECS.CASTLE) {
             if (this.canBuild(SPECS.PILGRIM) && pilgrimcount < 1) {
@@ -31,65 +57,105 @@ class MyRobot extends BCAbstractRobot {
                     if (robotsnear[this.me.x + alldirs[i][0]][this.me.y + alldirs[i][0]] == -1) {
                         this.log("Created pilgrim");
                         pilgrimcount++;
-                        return this.buildUnit(SPECS.PILGRIM, alldirs[i][0], alldirs[i][0]);
+                        return this.buildUnit(SPECS.PILGRIM, alldirs[i][0], alldirs[i][1]);
                     }
                 }
             }
         } else if (this.me.unit == SPECS.PILGRIM) {
+
+            if (castleloc == null) {
+                var tempmap = this.getVisibleRobotMap();
+                for (var i = 0; i < alldirs.length; i++) {
+                    var tempid = tempmap[this.me.y + alldirs[i][1]][this.me.x + alldirs[i][0]];
+                    if (tempid > 0) {
+                        var robottype = this.getRobot(tempid);
+                        if (robottype == SPECS.CASTLE) {
+                            castleloc = [this.me.x + alldirs[i][0]][this.me.y + alldirs[i][1]];
+                            break;
+                        }
+                    }
+                }
+            }
+
             if (karblocation == null) {
                 //find closest karbonite
-                var map = this.karbonite_map;
                 var queue = [];
-                queue.push([this.x, this.y]);
+                var visited = [];
+                queue.push([this.me.x, this.me.y]);
+                visited.push(this.hash(this.me.x, this.me.y));
                 while (queue.length != 0) {
                     var cur = queue.shift();
-                    if (map[cur[0]][cur[1]] == true) {
+                    if (this.karbonite_map[cur[1]][cur[0]] == true) {
                         karblocation = cur;
+                        this.log("FOUND KARBONITE");
+                        this.log(cur);
                         break;
                     }
                     for (var i = 0; i < alldirs.length; i++) {
-                        queue.push([cur[0] + alldirs[i][0], cur[1] + alldirs[i][1]]);
+                        if (visited.includes(this.hash(cur[0] + alldirs[i][0], cur[1] + alldirs[i][1])) == false) {
+                            queue.push([cur[0] + alldirs[i][0], cur[1] + alldirs[i][1]]);
+                            visited.push(this.hash(cur[0] + alldirs[i][0], cur[1] + alldirs[i][1]));
+                        }
                     }
                 }
             }
 
-            if (this.me.x != karblocation[0] || this.me.y != karblocation[1]) {
-            //move to karbonite
-                var dirvec = [karblocation[0] - this.me.x, karblocation[1] - this.me.y];
-                var mag = Math.sqrt(dirvec[0] * dirvec[0] + dirvec[1] * dirvec[1]);
-                dirvec[0] = Math.trunc(dirvec[0] / mag * SPECS.UNITS[SPECS.PILGRIM].SPEED);
-                dirvec[1] = Math.trunc(dirvec[1] / mag * SPECS.UNITS[SPECS.PILGRIM].SPEED);
-                this.log("Fucking move direction");
-                this.log(dirvec[0].toString());
-                this.log(dirvec[1].toString());
-
-                return this.move(dirvec[0] - this.me.x, dirvec[1] - this.me.y);
-            } else {
-                if (builtchurch == false && this.canBuild(SPECS.CHURCH)) {
-                    //build factory
+            if (builtchurch == false) {
+                if (this.canBuild(SPECS.CHURCH)) {
+                    if (this.me.x != karblocation[0] || this.me.y != karblocation[1]) {
+                        //move to karbonite
+                        return this.moveto(karblocation);
+                    }
+                    //build church
                     var robotsnear = this.getVisibleRobotMap();
 
                     for (var i = 0; i < alldirs.length; i++) {
-                        if (robotsnear[this.me.x + alldirs[i][0]][this.me.y + alldirs[i][0]] == -1) {
+                        if (robotsnear[this.me.y + alldirs[i][1]][this.me.x + alldirs[i][0]] != -1) {
                             builtchurch = true;
-                            log("BUILD FUCKING CHURCH");
-                            churchloc = [this.me.x + alldirs[i][0], this.me.y + alldirs[i][0]];
-                            return this.buildUnit(SPECS.CHURCH, alldirs[i][0], alldirs[i][0]);
+                            this.log("BUILD FUCKING CHURCH");
+                            churchloc = [alldirs[i][0], alldirs[i][1]];
+                            return this.buildUnit(SPECS.CHURCH, alldirs[i][0], alldirs[i][1]);
                         }
                     }
                 } else {
-                    //mine shit
+                    //fucking mine then move back to the castle ...
+
                     if (this.me.karbonite < SPECS.UNITS[SPECS.PILGRIM].KARBONITE_CAPACITY) {
-                        log("FUCKING MINE");
-                        return this.mine();
+                        //i didn't finish mining yet
+                        if (this.me.x != karblocation[0] || this.me.y != karblocation[1]) {
+                            //move to karbonite
+                            this.log("HEADING TOWARDS KARBONITE TO MINE FOR CASTLE");
+                            return this.moveto(karblocation);
+                        } else {
+                            this.log("MINING FOR CASTLE");
+                            return this.mine();
+                        }
                     } else {
-                        log("GIVE CHURCH KARB SHIT");
-                        return this.give(churchloc[0] - this.me.x, churchloc[1] - this.me.y, this.me.karbonite, this.me.fuel);
+                        //i gots the drugs, not move to castle and give drugs
+                        if (adjacent([this.me.x, this.me.y], castleloc)) {
+                            this.log("GIVING TO CASTLE");
+                            return this.give(castleloc[0] - this.me.x, castleloc[1] - this.me.y, this.me.karbonite, this.me.fuel);
+                        } else {
+                            this.log("MOVING BACK TO CASTLE TO GIVE DRUGS");
+                            return this.moveto(castleloc);
+                        }
                     }
                 }
+            } else {
+                if (adjacent([this.me.x, this.me.y], karblocation)) {
+                    //mine shit
+                    if (this.me.karbonite < SPECS.UNITS[SPECS.PILGRIM].KARBONITE_CAPACITY) {
+                        this.log("FUCKING MINE");
+                        return this.mine();
+                    } else {
+                        this.log("GIVE CHURCH KARB SHIT");
+                        return this.give(churchloc[0] - this.me.x, churchloc[1] - this.me.y, this.me.karbonite, this.me.fuel);
+                    }
+                } else {
+                    return this.moveto(karblocation);
+                }
+                
             }
-
-            
         }
 
     }
