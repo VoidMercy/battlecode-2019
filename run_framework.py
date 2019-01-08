21b5ac3ee8143c12a519fbe4f4ba89f0a45c4de4
neptunia <kevinshen100@gmail.com>
2019-01-07 23:38:35 -0500

    fix stuff

--- run_framework.py
+++ run_framework.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
-import osg
+import os
+import glob
 # TODO:
 # - have bots output the size of the map and starting conditions at beginning
 docstr = """Usage: framework.py foldername"""
@@ -15,14 +16,15 @@ exclude = "asdf asdfg"
 excludelist = exclude.split(' ')
 
 direc = sys.argv[1]
-for x in os.walk('.'):
-    if (x[0] not in excludelist) and os.path.isfile(x[0] + '/robot.js'):
+for x in glob.glob('*/'):
+    if (x not in excludelist) and os.path.isfile(x + 'robot.js'):
         # works
+        print("Testing %s"%x)
         wins = 0
         for i in range(best_of):
-            s = subprocess.check_output(["bc19run", "-r", direc, "-b", x[0]]).strip().split('\n')[-1]
+            s = subprocess.check_output(["bc19run", "-r", direc, "-b", x]).strip().split('\n')[-1]
             if " red " in s:
                 wins += 1
 
-        print("bot %s won %d/%d games against %s."%(direc,wins,best_of,x[0]))
+        print("bot %s won %d/%d games against %s."%(direc,wins,best_of,x))
 
