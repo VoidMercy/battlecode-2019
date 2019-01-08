385970e3b28717c8b6c48ae3392fba6fd352266b
VoidMercy <chingyuan.alex@gmail.com>
2019-01-08 10:27:51 -0500

    fix crusader testing code

--- singletilebfs/crusader.js
+++ singletilebfs/crusader.js
@@ -3,12 +3,12 @@ import {SPECS} from 'battlecode';
 //all variables
 var alldirs = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]]
 
+var target = null;
 
 
 export var Crusader = function() {
-    if (this.me.turn > 100) {
-        return this.moveto([(this.me.x + 30) % this.map.length, (this.me.y + 30) % this.map.length]);
-    } else {
-        return; 
-    }
+	if (target == null || (this.me.x == target[0] && this.me.y == target[1])) {
+		target = [(this.me.x + 30) % this.map[0].length, (this.me.y + 30) % this.map.length];
+	}
+	return this.moveto(target);
 }
