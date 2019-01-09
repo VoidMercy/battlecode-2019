7b0e52309ca8da412519f4ed35351af906a0b9d8
Sampriti Panda <sampdingo@hotmail.com>
2019-01-08 22:56:27 -0500

    Add Preacher

--- singletilebfs/preacher.js
+++ singletilebfs/preacher.js
@@ -1,6 +1,41 @@
 import {SPECS} from 'battlecode';
-import {alldirs} from 'constants.js'
+import {alldirs, preacherdirs, preacherattackdirs} from 'constants.js'
 
 export var Preacher = function() {
-    return;
+    var best_score = 0;
+    var best_score_locs;
+    var vismap = this.getVisibleRobotMap();
+    for(var i = 0; i < preacherdirs.length; i++) {
+        var attack_x = this.me.x + preacherdirs[i][0], attack_y = this.me.y + preacherdirs[i][1];
+        if(!this.validCoords(attack_x, attack_y)) continue;
+        var curr_score = 0;
+        var attack_count = 0;
+        for(var j = 0; j < preacherattackdirs.length; j+) {
+            var target_x = attack_x + preacherattackdirs[j][0], target_y = attack_y + preacherattackdirs[j][1];
+            var target_robot = vismap[target_y][target_x];
+            if(target_robot.team == this.me.team) {
+                if(target_x == this.me.x && target_y == this.me.y) {
+                  curr_score -= 60;
+                }
+                else if(target_robot.unit == SPECS.CASTLE || target_robot.unit == SPECS.CHURCH) {
+                  curr_score -= 80;
+                }
+                else {
+                  curr_score -= 30;
+                }
+            }
+            else {
+                attack_count++;
+                curr_score += 20;
+            }
+        }
+        if(attack_count >= 0 && curr_score >= best_score) {
+            best_score = curr_score;
+            best_score_locs = preacherdirs[i];
+        }
+    }
+    if(best_score_locs != undefined) {
+        return this.attack(best_score_locs);
+    }
+    return this._bc_null_action;
 }
