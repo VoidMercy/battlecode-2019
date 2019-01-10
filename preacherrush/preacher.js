5dfac43807eb9efcb3db8547ae065e3b881c2ee5
Alex <lin969@purdue.edu>
2019-01-09 22:53:22 -0500

    add preacher attack

--- preacherrush/preacher.js
+++ preacherrush/preacher.js
@@ -40,6 +40,50 @@ export var Preacher = function() {
         
     }
 
+    //attack
+    var best_score = 0;
+    var best_score_locs;
+    var vismap = this.getVisibleRobotMap();
+    for(var i = 0; i < preacherdirs.length; i++) {
+        var attack_x = this.me.x + preacherdirs[i][0], attack_y = this.me.y + preacherdirs[i][1];
+        if(!this.validCoords([attack_x, attack_y])) continue;
+        if(vismap[attack_y][attack_x] <= 0) continue;
+        var curr_score = 0;
+        var attack_count = 0;
+        for(var j = 0; j < preacherattackdirs.length; j++) {
+            var target_x = attack_x + preacherattackdirs[j][0], target_y = attack_y + preacherattackdirs[j][1];
+            if(!this.validCoords([target_x, target_y])) continue;
+            var target_robot = vismap[target_y][target_x];
+            if(target_robot <= 0) continue;
+            target_robot = this.getRobot(target_robot);
+            if(target_robot.team == this.me.team) {
+                if(target_x == this.me.x && target_y == this.me.y) {
+                  curr_score -= 60;
+                }
+                else if(target_robot.unit == SPECS.CASTLE || target_robot.unit == SPECS.CHURCH) {
+                  curr_score -= 80;
+                }
+                else if(target_robot.unit == SPECS.PILGRIM){
+                  curr_score -= 5;
+                } else {
+                    curr_score -= 20;
+                }
+            }
+            else {
+                attack_count++;
+                curr_score += 20;
+            }
+        }
+        if(attack_count >= 0 && curr_score >= best_score) {
+            best_score = curr_score;
+            best_score_locs = preacherdirs[i];
+        }
+    }
+    if(best_score_locs != undefined) {
+        this.log("PREACHER ATTACK");
+        return this.attack(...best_score_locs);
+    }
+
     //move towards target
     if (curtarget < enemylocs.length) {
         if (this.distance([this.me.x, this.me.y], enemylocs[curtarget]) <= 4) {
