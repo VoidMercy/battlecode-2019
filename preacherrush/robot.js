43d5514a9dd5b570724401f4cd6589855544ca8f
VoidMercy <chingyuan.alex@gmail.com>
2019-01-10 09:35:19 -0500

    remove useless files

--- preacherrush/robot.js
+++ preacherrush/robot.js
@@ -1,9 +1,6 @@
 import {BCAbstractRobot, SPECS} from 'battlecode';
-import { Crusader } from 'crusader.js'
 import { Castle } from 'castle.js'
-import { Church } from 'church.js'
 import { Pilgrim } from 'pilgrim.js'
-import { Prophet } from 'prophet.js'
 import { Preacher } from 'preacher.js'
 
 import {alldirs, crusaderdirs, otherdirs} from 'constants.js'
@@ -208,20 +205,11 @@ class MyRobot extends BCAbstractRobot {
             // first turn, calc symmetry
             symmetry = this.symmetricType();
         }
-        if (this.me.unit === SPECS.CRUSADER) {
-            return Crusader.call(this);
-        }
-        else if (this.me.unit === SPECS.CASTLE) {
+        if (this.me.unit === SPECS.CASTLE) {
             return Castle.call(this);
-        }
-        else if (this.me.unit === SPECS.CHURCH) {
-            return Church.call(this);
         }
         else if (this.me.unit === SPECS.PILGRIM) {
             return Pilgrim.call(this);
-        }
-        else if (this.me.unit === SPECS.PROPHET) {
-            return Prophet.call(this);
         }
         else if (this.me.unit === SPECS.PREACHER) {
             return Preacher.call(this);
