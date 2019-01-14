17edc0a09a67dda6cb2664b53c0258bdaef45e47
Sampriti Panda <sampdingo@hotmail.com>
2019-01-14 03:11:13 -0500

    Refactor some code in castle

--- lessworkers/castle.js
+++ lessworkers/castle.js
@@ -304,14 +304,11 @@ export var Castle = function() {
          this.karbonite > SPECS.UNITS[SPECS.PREACHER].CONSTRUCTION_KARBONITE * 3 &&
           this.fuel > SPECS.UNITS[SPECS.PREACHER].CONSTRUCTION_FUEL * 3))) {
         //can produce pilgrim
-        var robotsnear = this.getVisibleRobotMap();
-        for (var i = 0; i < alldirs.length; i++) {
-            var nextloc = [this.me.x + alldirs[i][0], this.me.y + alldirs[i][1]];
-            if (this.validCoords(nextloc) && robotsnear[nextloc[1]][nextloc[0]] == 0 && this.map[nextloc[1]][nextloc[0]] == true) {
-                this.log("Created pilgrim");
-                pilgrimcount++;
-                return this.buildUnit(SPECS.PILGRIM, alldirs[i][0], alldirs[i][1]);
-            }
+        var result = this.build(SPECS.PILGRIM);
+        if(result != null) {
+            pilgrimcount++;
+            this.log("Created pilgrim");
+            return this.buildUnit(SPECS.PILGRIM, result[0], result[1]);
         }
     }
     
