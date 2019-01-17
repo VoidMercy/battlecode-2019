81c16f8ebd57d0df4cbb67d56001c5b6f46208c3
Claude Zou <claude.zou.work@gmail.com>
2019-01-17 16:18:52 -0500

    Add lattice defense

---
+++ latticesice/church.js
@@ -0,0 +1,325 @@
+import {SPECS} from 'battlecode';
+import {alldirs, range10, lattices} from 'constants.js'
+
+//church vars
+var crusadercount = 0;
+var pilgrimcount = 0;
+var lategameUnitCount = 0;
+var underattack = false;
+var usedDefensePositions = []; //used for assigning where units go
+var curFlatEnemyVector = null;
+var turnsSinceLastReposition = 0;
+var lastenemyseen = [];
+var used_lattice_locs = [];
+var turns_since_used_lattice = [];
+
+export var Church = function() {
+
+    var prophetCount = 0;
+    var preacherCount = 0;
+    var crusaderCount = 0;
+    var robotsnear = this.getVisibleRobots();
+    var robotmap = this.getVisibleRobotMap();
+    var robot = null;
+    for (var i=0; i < robotsnear.length; i++) {
+        robot = robotsnear[i];
+        if (this.isVisible(robot) && robot.team == this.me.team) {
+            if (robot.unit == SPECS.PROPHET) {
+                prophetCount++;
+            } else if (robot.unit == SPECS.PREACHER) {
+                preacherCount++;
+            } else if (robot.unit == SPECS.CRUSADER) {
+                crusaderCount++;
+            }
+        }
+    }
+    //this.log(prophetCount);
+    if ((prophetCount + preacherCount + crusaderCount > 20 && this.fuel > 400) || (this.karbonite > 300)) {
+        this.log("signalling to go away ")
+        this.signal(69, 25);
+    }
+
+    for (var i = 0; i < used_lattice_locs.length; i++) {
+        if (turns_since_used_lattice[i] > 5) {
+            //pop since its been long enough probably
+            turns_since_used_lattice.splice(i, 1);
+            used_lattice_locs.splice(i, 1);
+        } else {
+            turns_since_used_lattice[i]++;
+        }
+    }
+
+    //defensive church code
+    var robot = null;
+    var numenemy = [0, 0, 0, 0, 0, 0]; // crusaders, prophets, preachers
+    var friendlies = [0, 0, 0, 0, 0, 0];
+    var defense_units = [0, 0, 0, 0, 0, 0];
+    var defense_robots = [];
+    var defensive_health = 0;
+    var enemy_health = 0;
+    var minDist = 9999999;
+    var closestEnemy = null;
+    for (var i = 0; i < robotsnear.length; i++) {
+        robot = robotsnear[i];
+        if (robot.team != this.me.team) {
+            numenemy[robot.unit]++;
+            enemy_health += robot.health;
+            if(this.isVisible(robot)) {
+                var dist = this.distance([this.me.x, this.me.y], [robot.x, robot.y])
+                if (dist < minDist && SPECS.UNITS[robot.unit].ATTACK_RADIUS != null) {
+                    minDist = dist;
+                    closestEnemy = robot;
+                    lastenemyseen = closestEnemy;
+                }
+            }
+        } else {
+            friendlies[robot.unit]++;
+            if (this.isVisible(robot) && this.distance([this.me.x, this.me.y], [robot.x, robot.y]) < 10) {
+                defense_units[robot.unit]++;
+                defense_robots.push(robot.unit);
+                if (robot.unit >= 3) {
+                    defensive_health += robot.health;
+                }
+            }
+        }
+    }
+
+    /*if (closestEnemy != null && this.fuel >= 10 && turnsSinceLastReposition >= 10) {
+        underattack = true;
+        var enemVector = [closestEnemy.x - this.me.x, closestEnemy.y - this.me.y];
+        for (var i = 0; i < 2; i++) {
+            enemVector[i] = Math.round(enemVector[i] / Math.max(...enemVector));
+        }
+        if (curFlatEnemyVector == null || curFlatEnemyVector[0] != enemVector[0] || curFlatEnemyVector[1] != enemVector[1]) {
+            //enemies are in a different direction, broadcast to erry1
+            curFlatEnemyVector = enemVector;
+            sicecoords = [];
+            var minDist = 9999;
+            var bestIndex = null;
+            for (var i = 0; i < range10.length; i++) {
+                var nextloc = [this.me.x + range10[i][0], this.me.y + range10[i][1]];
+                if (this.validCoords(nextloc) && this.map[nextloc[1]][nextloc[0]] && !usedDefensePositions.includes(i) && this.distance(nextloc, [closestEnemy.x, closestEnemy.y]) < minDist) {
+                    minDist = this.distance(nextloc, [closestEnemy.x, closestEnemy.y]);
+                    bestIndex = i;
+                }
+            }
+            if (bestIndex != null) {
+                var signal = this.generateRepositionSignal(range10[bestIndex]);
+                this.log("sending reposition signal!");
+                this.signal(signal, 10);
+                turnsSinceLastReposition = 0;
+            } else {
+                this.log("SOMETHING BORKED");
+            }
+            
+        }
+    }*/
+
+    if (numenemy[SPECS.CRUSADER] + numenemy[SPECS.PROPHET] + numenemy[SPECS.PREACHER] == 0 && underattack) {
+        underattack = false;
+        this.log("broadcast we are se-fu");
+        this.signal(6969, 10);
+    } else {
+        if (numenemy[SPECS.CRUSADER] + numenemy[SPECS.PREACHER] > defense_units[SPECS.PREACHER] || defensive_health < enemy_health) {
+            this.log("CREATE PREACHER FOR DEFENSE");
+            var result = null;
+            if (closestEnemy == null) {
+                this.log("THIS SHOULD RARELY HAPPEN");
+                result = this.buildNear(SPECS.PREACHER, [lastenemyseen.x, lastenemyseen.y]);
+            } else {
+                if (underattack && closestEnemy.unit == SPECS.PREACHER) {
+                    this.log("ohno");
+                    result = this.buildSpread(SPECS.PREACHER, [closestEnemy.x, closestEnemy.y]);
+                } else {
+                    result = this.buildNear(SPECS.PREACHER, [closestEnemy.x, closestEnemy.y]);
+                }
+            }
+
+            
+            if (result != null) {
+                var index = -1;
+                for (index = 0; index < lattices.length; index++) {
+                     var latticeloc = [this.me.x + lattices[index][0], this.me.y + lattices[index][1]];
+                     if (this.validCoords(latticeloc) /* coordinates are valid */ && 
+                        this.map[latticeloc[1]][latticeloc[0]] /* is passable terrain */ && 
+                        robotmap[latticeloc[1]][latticeloc[0]] == 0 /* not occupied */ &&
+                        !this.karbonite_map[latticeloc[1]][latticeloc[0]] /* not karbonite */ &&
+                        !this.fuel_map[latticeloc[1]][latticeloc[0]] /* not fuel */ &&
+                        !used_lattice_locs.includes(index) /* havent used in past few turns */) {
+                        used_lattice_locs.push(index);
+                        turns_since_used_lattice.push(0);
+                        break; //found tile :O
+                    }
+                }
+                //send signal for starting pos
+                if (index != -1) {
+                    var signal = this.generateDefenseInitialSignal(lattices[index], closestEnemy.unit);
+                    this.log("sent: ");
+                    this.log(lattices[index]);
+                    this.log(signal);
+                    this.signal(signal, 2); // todo maybe: check if required r^2 is 1
+                }
+                return this.buildUnit(SPECS.PREACHER, result[0], result[1]);
+            }
+        } else if ((numenemy[SPECS.PROPHET]) * 2 > defense_units[SPECS.PROPHET] || defense_robots[SPECS.PROPHET] + defense_robots[SPECS.PREACHER] == 0) {
+            //produce preacher to counter crusader
+            this.log("CREATE PROPHET FOR DEFENSE");
+            var result = this.buildNear(SPECS.PROPHET, [closestEnemy.x, closestEnemy.y]);
+            if (result != null) {
+                var index = -1;
+                for (index = 0; index < lattices.length; index++) {
+                     var latticeloc = [this.me.x + lattices[index][0], this.me.y + lattices[index][1]];
+                     if (this.validCoords(latticeloc) /* coordinates are valid */ && 
+                        this.map[latticeloc[1]][latticeloc[0]] /* is passable terrain */ && 
+                        robotmap[latticeloc[1]][latticeloc[0]] == 0 /* not occupied */ &&
+                        !this.karbonite_map[latticeloc[1]][latticeloc[0]] /* not karbonite */ &&
+                        !this.fuel_map[latticeloc[1]][latticeloc[0]] /* not fuel */ &&
+                        !used_lattice_locs.includes(index) /* havent used in past few turns */) {
+                        used_lattice_locs.push(index);
+                        turns_since_used_lattice.push(0);
+                        break; //found tile :O
+                    }
+                }
+                //send signal for starting pos
+                if (index != -1) {
+                    var signal = this.generateDefenseInitialSignal(lattices[index], closestEnemy.unit);
+                    this.log("sent: ");
+                    this.log(lattices[index]);
+                    this.log(signal);
+                    this.signal(signal, 2); // todo maybe: check if required r^2 is 1
+                }
+                return this.buildUnit(SPECS.PROPHET, result[0], result[1]);
+            }
+        } 
+        /*
+        if (numenemy[SPECS.CRUSADER] + numenemy[SPECS.PROPHET] + numenemy[SPECS.PREACHER] > defense_units[SPECS.PROPHET]) {
+            //produce prophet to counter attack
+            var result = this.build(SPECS.PROPHET);
+            if (result != null) {
+                return result;
+            }
+        }*/
+    }
+
+    if (!underattack && closestEnemy == null) {
+        //produce these even tho not "under attack" technically
+        if ((numenemy[SPECS.CASTLE] + numenemy[SPECS.CHURCH]) * 2 > defense_units[SPECS.PREACHER]) {
+            //spawn preacher for enemy castles/churches
+            this.log("CREATE PREACHER FOR ATTACKING ENEMY CHURCH/CASTLE");
+            var result = this.build(SPECS.PREACHER);
+            if (result != null) {
+                var index = -1;
+                for (index = 0; index < lattices.length; index++) {
+                     var latticeloc = [this.me.x + lattices[index][0], this.me.y + lattices[index][1]];
+                     if (this.validCoords(latticeloc) /* coordinates are valid */ && 
+                        this.map[latticeloc[1]][latticeloc[0]] /* is passable terrain */ && 
+                        robotmap[latticeloc[1]][latticeloc[0]] == 0 /* not occupied */ &&
+                        !this.karbonite_map[latticeloc[1]][latticeloc[0]] /* not karbonite */ &&
+                        !this.fuel_map[latticeloc[1]][latticeloc[0]] /* not fuel */ &&
+                        !used_lattice_locs.includes(index) /* havent used in past few turns */) {
+                        used_lattice_locs.push(index);
+                        turns_since_used_lattice.push(0);
+                        break; //found tile :O
+                    }
+                }
+                //send signal for starting pos
+                if (index != -1) {
+                    var signal = this.generateDefenseInitialSignal(lattices[index], SPECS.CHURCH);
+                    this.log("sent: ");
+                    this.log(lattices[index]);
+                    this.log(signal);
+                    this.signal(signal, 2); // todo maybe: check if required r^2 is 1
+                }
+                return this.buildUnit(SPECS.PREACHER, result[0], result[1]);
+            }
+        } else if (numenemy[SPECS.PILGRIM] > friendlies[SPECS.CRUSADER]*5) {
+            //spawn crusaders for enemy pilgrims
+            this.log("CREATE crusader FOR ATTACKING ENEMY PILGRIM");
+            var result = this.build(SPECS.CRUSADER);
+            if (result != null) {
+                var index = -1;
+                for (index = 0; index < lattices.length; index++) {
+                     var latticeloc = [this.me.x + lattices[index][0], this.me.y + lattices[index][1]];
+                     if (this.validCoords(latticeloc) /* coordinates are valid */ && 
+                        this.map[latticeloc[1]][latticeloc[0]] /* is passable terrain */ && 
+                        robotmap[latticeloc[1]][latticeloc[0]] == 0 /* not occupied */ &&
+                        !this.karbonite_map[latticeloc[1]][latticeloc[0]] /* not karbonite */ &&
+                        !this.fuel_map[latticeloc[1]][latticeloc[0]] /* not fuel */ &&
+                        !used_lattice_locs.includes(index) /* havent used in past few turns */) {
+                        used_lattice_locs.push(index);
+                        turns_since_used_lattice.push(0);
+                        break; //found tile :O
+                    }
+                }
+                //send signal for starting pos
+                if (index != -1) {
+                    var signal = this.generateDefenseInitialSignal(lattices[index], SPECS.PILGRIM);
+                    this.log("sent: ");
+                    this.log(lattices[index]);
+                    this.log(signal);
+                    this.signal(signal, 2); // todo maybe: check if required r^2 is 1
+                }
+                return this.buildUnit(SPECS.CRUSADER, result[0], result[1]);
+            }
+        }
+    }
+
+    //offensive code lategame
+    if (this.karbonite > 250 && this.fuel > 500) {
+        // lmoa build a prophet
+        lategameUnitCount++;
+        var unitBuilder;
+        if (lategameUnitCount % 3 == 0) {
+            //make preacher
+            unitBuilder = SPECS.PREACHER;
+        } else {
+            //make prophet
+            unitBuilder = SPECS.PROPHET;
+        }
+        this.log("BUILDING RANGER!!!")
+        var result = this.build(unitBuilder);
+        if (result != null) {
+            var index = -1;
+            for (index = 0; index < lattices.length; index++) {
+                var latticeloc = [this.me.x + lattices[index][0], this.me.y + lattices[index][1]];
+                if (this.validCoords(latticeloc) /* coordinates are valid */ && 
+                    this.map[latticeloc[1]][latticeloc[0]] /* is passable terrain */ && 
+                    robotmap[latticeloc[1]][latticeloc[0]] == 0 /* not occupied */ &&
+                    !this.karbonite_map[latticeloc[1]][latticeloc[0]] /* not karbonite */ &&
+                    !this.fuel_map[latticeloc[1]][latticeloc[0]] /* not fuel */ &&
+                    !used_lattice_locs.includes(index) /* havent used in past few turns */) {
+                    used_lattice_locs.push(index);
+                    turns_since_used_lattice.push(0);
+                    break; //found tile :O
+                }
+            }
+            //send signal for starting pos
+            if (index != -1) {
+                var signal = this.generateInitialPosSignalVal(lattices[index]);
+                this.log("sent: ");
+                this.log(lattices[index]);
+                this.log(signal);
+                this.signal(signal, 2); // todo maybe: check if required r^2 is 1
+                return this.buildUnit(unitBuilder, result[0], result[1]);
+            }
+        }
+    }
+
+
+    /*if (crusadercount < 3 || pilgrimcount > 0) {
+        if (this.karbonite > SPECS.UNITS[SPECS.CHURCH].CONSTRUCTION_KARBONITE + SPECS.UNITS[SPECS.CRUSADER].CONSTRUCTION_KARBONITE && this.fuel > SPECS.UNITS[SPECS.CHURCH].CONSTRUCTION_FUEL) {
+            var result = this.build(SPECS.CRUSADER);
+            if (result != null) {
+                crusadercount++;
+                return result;
+            }
+        }
+    } else if (pilgrimcount == 0) {
+        var result = this.build(SPECS.PILGRIM);
+        if (result != null) {
+            pilgrimcount++;
+            return result;
+        }
+    }*/
+    return this._bc_null_action();
+}
---
+++ latticesice/preacher.js
@@ -0,0 +1,242 @@
+import {SPECS} from 'battlecode';
+import {alldirs, preacherdirs, preacherattackdirs, otherdirs} from 'constants.js'
+
+var target = null;
+var castleLoc = null; 
+var tempTarget = null;
+var offenseFlag =  0;
+var reachedTarget = false;
+var altTargets;
+var relStartPos = null;
+var targetNum = 0;
+var underattack = false;
+var therearepreachers = false;
+var stuckcount = 0;
+
+export var Preacher = function() {
+
+    var tempmap = this.getVisibleRobotMap();
+    if (this.me.turn == 1) {
+        //first turn, find location of church/castle and obtain initial pos
+        for (var i = 0; i < alldirs.length; i++) {
+            var nextLoc = [this.me.x + alldirs[i][0], this.me.y + alldirs[i][1]];
+            if (this.validCoords(nextLoc)) {
+                var robot = this.getRobot(tempmap[nextLoc[1]][nextLoc[0]]);
+                if (tempmap[nextLoc[1]][nextLoc[0]] > 0 &&
+                (robot.unit == SPECS.CASTLE || robot.unit == SPECS.CHURCH)) {
+                    //church/castle i spawned on
+                    if (robot.signal != -1) {
+                        this.log("SIGNAL");
+                        this.log(robot.signal);
+                        relStartPos = this.decodeSignal(robot.signal);
+                        target = [robot.x + relStartPos[0], robot.y + relStartPos[1]];
+                        this.log("Received: ");
+                        this.log(relStartPos);
+                    } else {
+                        this.log("NO SIGNAL!");
+                        target = nextLoc;
+                    }
+                    castleLoc = nextLoc;
+                    break;
+                }
+            }
+        }
+        this.log(castleLoc);
+    }
+
+    if (castleLoc != null && tempmap[castleLoc[1]][castleLoc[0]] > 0) { //listen for reposition signal
+        var castle = this.getRobot(tempmap[castleLoc[1]][castleLoc[0]]);
+        if (castle.signal != -1 && castle.signal % 8 == 6) {
+            this.log("REPOSITIONAL SIGNAL");
+            this.log(castle.signal);
+            relStartPos = this.decodeSignal(castle.signal);
+            target = [castle.x + relStartPos[0], castle.y + relStartPos[1]];
+            if (relStartPos[2] == SPECS.PREACHER) {
+                therearepreachers = true;
+            }
+            reachedTarget = false;
+            underattack = true;
+            this.log("Received: ");
+            this.log(target);
+        } else if (castle.signal != -1 && castle.signal == 6969) {
+            this.log("we are not under attack anymore");
+            underattack = false;
+        }
+    }
+/*
+    if (tempTarget != null && this.distance(tempTarget, [this.me.x, this.me.y]) <= 4) {
+        //close enough to temp target, we are probably aggrod onto enemy and/or enemy is dead now
+        tempTarget = null;
+    }*/
+
+    var robotsnear = this.getVisibleRobots();
+    var robot = null;
+    var enemyPreachers = [];
+    for (var i=0; i < robotsnear.length; i++) {
+        robot = robotsnear[i];
+        if (this.isRadioing(robot) && robot.signal == 69 && offenseFlag != 1) {
+            offenseFlag=1;
+            reachedTarget = false;
+            this.log("received signal!");
+            target = null;
+            break;
+        }
+        if (this.isVisible(robot) && robotsnear[i].team != this.me.team && robotsnear[i].unit == SPECS.PREACHER) {
+            enemyPreachers.push(robot);
+        }
+    }
+
+    if (offenseFlag == 1) {
+        if (target == null) {
+            var opposite = this.oppositeCoords([this.me.x, this.me.y]);
+            //todo: use comms to get enemy castle locations
+            altTargets = [opposite,[this.map.length - this.me.x, this.map.length - this.me.y],[this.map.length - opposite[0], this.map.length - opposite[1]], [Math.floor(this.map.length / 2), Math.floor(this.map.length / 2)], [0,0], [0, this.map.length-8], [this.map.length-8, this.map.length-8], [this.map.length-8, 0], [this.me.x, this.me.y]];
+            for (var i = 0; i < altTargets.length; i++) {
+                if (!this.validCoords([altTargets[i][0], altTargets[i][1]]) || !this.map[altTargets[i][1]][altTargets[i][0]]) {
+                    altTargets.splice(i, 1); //remove impassable tile targets
+                }
+            }
+            target = altTargets[targetNum];
+        } else if (this.distance(target, [this.me.x, this.me.y]) <= SPECS.UNITS[this.me.unit].SPEED) {
+            reachedTarget = true;
+        }
+    }
+
+    //todo: use different micro for defense and offense LOL
+    //aka if defensive, use castle for extra vision
+    var best_score = 0;
+    var best_score_locs;
+    var vismap = this.getVisibleRobotMap();
+    for(var i = 0; i < preacherdirs.length; i++) {
+        var attack_x = this.me.x + preacherdirs[i][0], attack_y = this.me.y + preacherdirs[i][1];
+        if(!this.canAttack([attack_x, attack_y])) continue;
+        var curr_score = 0;
+        var attack_count = 0;
+        for(var j = 0; j < preacherattackdirs.length; j++) {
+            var target_x = attack_x + preacherattackdirs[j][0], target_y = attack_y + preacherattackdirs[j][1];
+            if(!this.validCoords([target_x, target_y])) continue;
+            var target_robot = vismap[target_y][target_x];
+            if(target_robot <= 0) continue;
+            target_robot = this.getRobot(target_robot);
+            if(target_robot.team == this.me.team) {
+                if(target_x == this.me.x && target_y == this.me.y) {
+                  curr_score -= 60;
+                }
+                else if(target_robot.unit == SPECS.CASTLE || target_robot.unit == SPECS.CHURCH) {
+                  curr_score -= 80;
+                }
+                else if(target_robot.unit == SPECS.PILGRIM){
+                  curr_score -= 5;
+                } else {
+                  curr_score -= 20;
+                }
+            }
+            else {
+                attack_count++;
+                if(target_robot.unit == SPECS.CASTLE) {
+                  curr_score += 60;
+                }
+                else if(target_robot.unit == SPECS.CHURCH) {
+                  curr_score += 40;
+                }
+                else {
+                  curr_score += 20;
+                }
+            }
+        }
+        if(attack_count > 0 && curr_score >= best_score) {
+            best_score = curr_score;
+            best_score_locs = preacherdirs[i];
+        }
+    }
+
+    if(best_score_locs != undefined) {
+        this.log("PREACHER ATTACK");
+        return this.attack(...best_score_locs);
+    }
+
+    /*
+    //if signal says preacher exists, spread out :O
+    //this.log(typeof relStartPos);
+    if (underattack && therearepreachers && offenseFlag == 0) {
+        //only do this in defensive mode
+        var needSpread = false;
+        for (var i = 0; i < alldirs.length; i++) {
+            //first check if adjacent to units to avoid never attacking lol
+            var nextloc = [this.me.x + alldirs[i][0], this.me.y + alldirs[i][1]];
+            if (this.validCoords(nextloc) && tempmap[nextloc[1]][nextloc[0]] > 0) {
+                if (this.getRobot(tempmap[nextloc[1]][nextloc[0]]).team == this.me.team && this.getRobot(tempmap[nextloc[1]][nextloc[0]]).unit >= 3) {
+                    needSpread = true;
+                    break;
+                }
+            }
+        }
+        this.log("SPREAD OUT NIGGERS");
+        if (needSpread) {
+            var maxdist = -1;
+            var bestpos = null;
+            for (var i = 0; i < otherdirs.length; i++) {
+                var nextloc = [this.me.x + otherdirs[i][0], this.me.y + otherdirs[i][1]];
+                if (this.validCoords(nextloc) && tempmap[nextloc[1]][nextloc[0]] > 0) {
+                    var good = true;
+                    for (var j = 0; j < robotsnear.length; j++) {
+                        if (this.distance(nextloc, [robotsnear[j].x, robotsnear[j].y]) <= 2) {
+                            good = false;
+                            break;
+                        }
+                    }
+                    if (good) {
+                        var dist = this.distance(nextloc, target);
+                        if (dist > maxdist) {
+                            maxdist = dist;
+                            bestpos = nextloc;
+                        }
+                    }
+                }
+            }
+            this.log("SPREAD THAT PUSSY");
+            this.move(bestpos[0] - this.me.x, bestpos[1] - this.me.y);
+        }
+    }*/
+
+    /*
+    if (tempTarget != null) {
+        this.log("repositioning ecks dee");
+        var move = this.nopreachermoveto(tempTarget);
+        if (move != null) {
+            this.move(...move);
+        } else {
+            stuckcount++;
+            if (stuckcount >= 4) {
+                target = [this.me.x, this.me.y];
+            }
+        }
+    }*/
+
+    if (reachedTarget || (target != null && !this.validCoords(target))) {
+        //this.log("Switching targets!");
+        reachedTarget = false;
+        targetNum = (targetNum + 1) % altTargets.length;
+        target = altTargets[targetNum];
+    }
+
+    if (target != null && (this.me.x != target[0] || this.me.y != target[1])) {
+        //this.log("preacher moving to defensive position!");
+        // this.log("TRY TO MOVE");
+        if (offenseFlag) {
+            return this.moveto(target);
+        }
+        var move = this.nopreachermoveto(target);
+        if (move != null) {
+            stuckcount = 0;
+            return this.move(...move);
+        } else {
+            stuckcount++;
+            if (stuckcount >= 6) {
+                target = [this.me.x, this.me.y];
+            }
+        }
+    }
+    
+    return this._bc_null_action();
+}
