429c027dd04120b7f6e3332832cd2626195f05b6
Alex <lin969@purdue.edu>
2019-01-23 20:31:01 -0500

    preacher bugfix

--- worker-strat/castle.js
+++ worker-strat/castle.js
@@ -206,7 +206,7 @@ function find_target_stronghold() {
 	var closest_castle = null;
 	for (var i = 0; i < plannedchurches.length; i++) {
 		for (var j = 0; j < castlelocs.length; j++) {
-			if (plannedchurches[i] == null) {
+			if (plannedchurches[i] == null || plannedchurches[i][0] == VERY_CONTESTED) {
 				continue;
 			}
 			tempdist = this.distance(plannedchurches[i][1], castlelocs[j]);
